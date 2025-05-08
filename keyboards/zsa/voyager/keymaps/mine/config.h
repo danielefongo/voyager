463c900ad44a7a8622a12334026cf494263a19c6
danielefongo <danielefongo@gmail.com>
2025-05-08 17:19:22 +0200

    feat: add autoshift

--- keyboards/zsa/voyager/keymaps/mine/config.h
+++ keyboards/zsa/voyager/keymaps/mine/config.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#define TAPPING_TERM 200
+#define QUICK_TAP_TERM 0
+
+#define RETRO_SHIFT 500
+#define AUTO_SHIFT_TIMEOUT 200
+#define NO_AUTO_SHIFT_SYMBOLS
+#define NO_AUTO_SHIFT_NUMERIC
+
 #ifdef VIA_ENABLE
 #define VIAL_KEYBOARD_UID {0xFD, 0x2F, 0x7F, 0x8A, 0x39, 0x07, 0xF7, 0xDB}
 
