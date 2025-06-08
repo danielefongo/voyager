4d7e0fb050d4797b1b220f618fed516730582d5d
danielefongo <danielefongo@gmail.com>
2025-06-08 09:29:08 +0200

    feat: use layer enum

--- keyboards/zsa/voyager/keymaps/mine/keymap.c
+++ keyboards/zsa/voyager/keymaps/mine/keymap.c
@@ -2,17 +2,24 @@
 
 #define __ KC_NO
 
+enum Layer {
+    MAIN,
+    SYMBOLS,
+    NAVIGATION,
+    EXTRA
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT(
-      __      , KC_1         , KC_2         , KC_3         , KC_4          , KC_5           ,     KC_6    , KC_7          , KC_8         , KC_9         , KC_0         , __        ,
+[MAIN] = LAYOUT(
+      TO(4)   , KC_1         , KC_2         , KC_3         , KC_4          , KC_5           ,     KC_6    , KC_7          , KC_8         , KC_9         , KC_0         , __        ,
       KC_TAB  , KC_Q         , KC_W         , KC_F         , KC_P          , KC_B           ,     KC_J    , KC_L          , KC_U         , KC_Y         , KC_SEMICOLON , S(KC_QUOT),
       KC_COLN , LSFT_T(KC_A) , LALT_T(KC_R) , LGUI_T(KC_S) , LCTL_T(KC_T)  , KC_G           ,     KC_M    , RCTL_T(KC_N)  , RGUI_T(KC_E) , RALT_T(KC_I) , RSFT_T(KC_O) , KC_QUOT   ,
       KC_EQL  , KC_Z         , KC_X         , KC_C         , KC_D          , KC_V           ,     KC_K    , KC_H          , KC_COMMA     , KC_DOT       , KC_SLSH      , KC_UNDS   ,
                                                              LT(1, KC_ENT) , LSFT_T(KC_ESC) ,     KC_BSPC , LT(2, KC_SPC)
 ),
 
-[1] = LAYOUT(
+[SYMBOLS] = LAYOUT(
       __      , __      , __      , __      , __      , __      ,     __      , __            , __      , __       , __      , __          ,
       KC_PERC , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,     KC_6    , KC_7          , KC_8    , KC_9     , KC_0    , KC_BACKSLASH,
       KC_AT   , KC_PIPE , KC_MINS , KC_LCBR , KC_RCBR , KC_QUES ,     KC_EXLM , KC_LPRN       , KC_RPRN , KC_SLASH , KC_AMPR , KC_DQUO     ,
@@ -20,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                               __      , __      ,     KC_BSPC , LT(3, KC_SPC)
 ),
 
-[2] = LAYOUT(
+[NAVIGATION] = LAYOUT(
       __ , __      , __      , __      , __      , __      ,     __      , __      , __       , __       , __       , __     ,
       __ , __      , __      , __      , __      , __      ,     KC_1    , KC_2    , KC_3     , KC_4     , __       , __     ,
       __ , KC_LSFT , KC_LALT , KC_LGUI , KC_LCTL , __      ,     KC_LEFT , KC_DOWN , KC_UP    , KC_RIGHT , __       , __     ,
@@ -28,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                          MO(3)   , KC_LSFT ,     __      , __
 ),
 
-[3] = LAYOUT(
+[EXTRA] = LAYOUT(
       __      , __      , __      , __      , __      , __ ,     __      , __        , __           , __     , __ , __,
       __      , RM_TOGG , KC_VOLD , KC_VOLU , KC_MUTE , __ ,     __      , __        , __           , __     , __ , __,
       KC_LSFT , KC_MPRV , KC_MNXT , KC_MSTP , KC_MPLY , __ ,     KC_HOME , KC_PGDN   , KC_PGUP      , KC_END , __ , __,
