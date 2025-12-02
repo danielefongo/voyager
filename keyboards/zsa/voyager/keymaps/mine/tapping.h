b640f66afbfaac776159dadd459385d663cf94ba
danielefongo <danielefongo@gmail.com>
2025-12-02 22:13:41 +0100

    feat: use variable flow tap term

--- keyboards/zsa/voyager/keymaps/mine/tapping.h
+++ keyboards/zsa/voyager/keymaps/mine/tapping.h
@@ -36,4 +36,13 @@ bool is_flow_tap_key(uint16_t keycode) {
     }
     return false;
 }
+
+uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_keycode) {
+    if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
+        uint8_t row = record->event.key.row;
+        uint8_t col = record->event.key.col;
+        return FLOW_TAP_TERM + tapping_term_delays[row][col];
+    }
+    return 0;
+}
 #endif
