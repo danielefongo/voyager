907a675735d00f2b4a09e83e3944cfc44eaa2a25
danielefongo <danielefongo@gmail.com>
2025-12-04 07:05:55 +0100

    feat: remove flow tap because it brings more discomfort than benefits

--- keyboards/zsa/voyager/keymaps/mine/config.h
+++ keyboards/zsa/voyager/keymaps/mine/config.h
@@ -3,7 +3,6 @@
 #define PERMISSIVE_HOLD
 
 #define CHORDAL_HOLD
-#define FLOW_TAP_TERM 200
 
 #define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
--- keyboards/zsa/voyager/keymaps/mine/tapping.h
+++ keyboards/zsa/voyager/keymaps/mine/tapping.h
@@ -20,29 +20,3 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     return TAPPING_TERM + tapping_term_delays[row][col];
 }
 #endif
-
-#ifdef FLOW_TAP_TERM
-bool is_flow_tap_key(uint16_t keycode) {
-    if ((get_mods() & (MOD_MASK_CG | MOD_BIT_LALT)) != 0) {
-        return false;
-    }
-    switch (get_tap_keycode(keycode)) {
-        case KC_A ... KC_Z:
-        case KC_DOT:
-        case KC_COMM:
-        case KC_SCLN:
-        case KC_SLSH:
-            return true;
-    }
-    return false;
-}
-
-uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_keycode) {
-    if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
-        uint8_t row = record->event.key.row;
-        uint8_t col = record->event.key.col;
-        return FLOW_TAP_TERM + tapping_term_delays[row][col];
-    }
-    return 0;
-}
-#endif
