2b93443830cc8eeb05b7d7364f663d03ebd6f423
danielefongo <danielefongo@gmail.com>
2025-12-10 09:09:45 +0100

    feat: use hrm shifts to enable caps lock

--- keyboards/zsa/voyager/keymaps/mine/combos.h
+++ keyboards/zsa/voyager/keymaps/mine/combos.h
@@ -1,3 +1,4 @@
+#include "constants.h"
 #include QMK_KEYBOARD_H // needed
 
 #ifdef COMBO_ENABLE
@@ -6,9 +7,9 @@ enum combos {
     COMBO_LENGTH
 };
 
-const uint16_t PROGMEM c_capsword[] = {KC_H, KC_COMM, COMBO_END};
+const uint16_t PROGMEM c_capslock[] = {KC_A_S, KC_O_S, COMBO_END};
 
 combo_t key_combos[] = {
-    [C_CAPSLOCK] = COMBO(c_capsword, KC_CAPS_LOCK),
+    [C_CAPSLOCK] = COMBO(c_capslock, KC_CAPS_LOCK),
 };
 #endif
