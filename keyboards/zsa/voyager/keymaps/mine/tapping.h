3af8e3ff385843be77b58878e6bbdefe36d9227b
danielefongo <danielefongo@gmail.com>
2026-02-05 11:35:59 +0100

    fix: tapping term usage

--- keyboards/zsa/voyager/keymaps/mine/tapping.h
+++ keyboards/zsa/voyager/keymaps/mine/tapping.h
@@ -2,7 +2,7 @@
 #include "constants.h"
 #include QMK_KEYBOARD_H // needed
 
-#ifdef TAPPING_TERM_PER_KEY_2
+#ifdef TAPPING_TERM_PER_KEY
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     uint8_t row = record->event.key.row;
     uint8_t col = record->event.key.col;
